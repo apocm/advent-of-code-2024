839d4bea2dd04117a1d48a85bb087e166a3cd7be
Alyssa Gangai <alyssa@iheartjane.com>
2024-12-05 12:03:14 -0500

    dec 1, puzzle 2

--- location_finder.rb
+++ location_finder.rb
@@ -43,6 +43,54 @@ class LocationFinder
   def self.delta(a,b)
     (a-b).abs
   end
+
+  def self.scan(listA = nil, listB = nil, debug: false)
+    listA ||= [3, 4, 2, 1, 3, 3]
+    listB ||= [4, 3, 5, 3, 9, 3]
+    counts = {}
+
+    listA.each do |a|
+      puts "scanning for #{a}" if debug
+
+      loop do
+        # find first occurence of value in list A
+        index = listB.index(a)
+        break if index == nil || listB.length == 0
+
+        # init or increment the count
+        if counts[a]
+          counts[a] +=1
+        else
+          counts[a] = 1
+        end
+
+        listB.delete_at(index)
+      end
+      puts "count #{counts[a]}" if debug
+    end
+
+    similarity_score = self.similarity(listA, counts)
+
+    puts "similarity_score: #{similarity_score}"
+  end
+
+  def self.similarity(listA, counts)
+    sum = 0
+
+    listA.each do |a|
+      next if !counts[a]
+      sum += (a * counts[a])
+    end
+
+    sum
+  end
 end
 
-LocationFinder.calculate_distance(Lists.list_a, Lists.list_b, debug: false)
+# # 01/01 Puzzle 1
+# LocationFinder.calculate_distance(Lists.list_a, Lists.list_b, debug: false)
+
+# 01/01 Puzzle 2
+LocationFinder.scan(Lists.list_a, Lists.list_b, debug: false)
+
+# This time, you'll need to figure out exactly how often each number from the left list appears in the right list.
+#Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
